c04cee2e6987e7fbed9475c6fe293a27e5329042
mfgrenchicago <plake@mfgren.org>
2025-06-09 22:40:38 -0500

    Initial Commit

---
+++ CIRN-2025/CIRN_2025App.swift
@@ -0,0 +1,17 @@
+//
+//  CIRN_2025App.swift
+//  CIRN-2025
+//
+//  Created by Pauline Lake on 6/9/25.
+//
+
+import SwiftUI
+
+@main
+struct CIRN_2025App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CIRN-2025/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CIRN-2025
+//
+//  Created by Pauline Lake on 6/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
