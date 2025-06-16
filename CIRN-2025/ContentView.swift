0a9b986960b1e083ccd5875042142e5eb3a4ebba
Nerdy Yawdie <di_muirhead@hotmail.com>
2025-06-16 14:31:21 -0400

    feat: Adding TabView

--- CIRN-2025/ContentView.swift
+++ CIRN-2025/ContentView.swift
@@ -9,21 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            
-            Text("Hello, DPI Interns")
-            
-            Button {
-                
-            } label: {
-                Text("Press Me")
-            }
-
-        }
-        .padding()
+		TabView {
+			
+		}
     }
 }
 
