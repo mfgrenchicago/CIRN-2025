12c7819133feff0181f137c7bc11c2fccdaa0f43
apere258 <165004346+apere258@users.noreply.github.com>
2025-06-16 14:17:05 -0500

    Feat: Added Manufacturing Companies View to Tab View

---
+++ CIRN-2025/Manufacturing Companies/ManufacturingCompaniesView.swift
@@ -0,0 +1,18 @@
+//
+//  ManufacturingCompaniesView.swift
+//  CIRN-2025
+//
+//  Created by Amado Perez - on 6/16/25.
+//
+
+import SwiftUI
+
+struct ManufacturingCompaniesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ManufacturingCompaniesView()
+}
