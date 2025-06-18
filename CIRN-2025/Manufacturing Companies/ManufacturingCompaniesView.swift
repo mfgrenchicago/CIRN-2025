de66e08e676e950f69fc05103c32883a4b94c189
apere258 <165004346+apere258@users.noreply.github.com>
2025-06-18 11:19:15 -0500

    Feat: Added a list for 5 manufacturing companies, developed a struck in order to store multiple information in one variable. Bolded the manufacturing company, added two footnotes in order to show a small description and the contact information.

--- CIRN-2025/Manufacturing Companies/ManufacturingCompaniesView.swift
+++ CIRN-2025/Manufacturing Companies/ManufacturingCompaniesView.swift
@@ -1,21 +1,55 @@
-//
-//  ManufacturingCompaniesView.swift
-//  CIRN-2025
-//
-//  Created by Amado Perez - on 6/16/25.
-//
+    //
+    //  ManufacturingCompaniesView.swift
+    //  CIRN-2025
+    //
+    //  Created by Amado Perez - on 6/16/25.
+    //
 
-import SwiftUI
+    import SwiftUI
 
-struct ManufacturingCompaniesView: View {
-    var body: some View {
-        NavigationStack {
-            Text("Manufacturing")
-                .navigationTitle("Manufactures")
+    struct ManufacturingCompaniesView: View {
+        //struck that creates a variable that could store multiple infromation inside
+        //Allowing to add multiple things in one line
+        struct Company: Identifiable {
+            let id = UUID()
+            let name: String
+            let description: String
+            let email: String
         }
+        //added information into the variable/struck
+        @State var companys: [Company] = [
+            Company(name: "Company", description: "++++", email: "Contact info"),
+            Company(name: "Archer-Daniels-Midland Company", description: "++++", email: "Contact info"),
+            Company(name: "AbbVie Inc.", description: "++++", email: "Contact info"),
+            Company(name: "Mondelez International, Inc.", description: "++++", email: "Contact info"),
+            Company(name: "GE HealthCare Technologies Inc.", description: "++++", email: "Contact info")
+            ]
+        
+        
+        var body: some View {
+            NavigationStack {
+                //edits the information from the struc
+                List (companys) { company in
+                    VStack(alignment: .leading) {
+                        //bolds the company name
+                        Text(company.name)
+                            .bold()
+                            //changes the information into a footnote/subscript and changes the color
+                        Text(company.description)
+                            .font(.footnote)
+                            .foregroundColor(.gray)
+                        Text(company.email)
+                            .font(.footnote)
+                            .foregroundColor(Color.blue)
+                    }
+                }
+                //adds a title on the top left corner.
+                .navigationTitle("Manufacturers")
+            }
+                    
+        }
+                //adds a title on the top left corner.
     }
-}
-
-#Preview {
-    ManufacturingCompaniesView()
-}
+    #Preview {
+        ManufacturingCompaniesView()
+    }
