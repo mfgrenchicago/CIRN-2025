8c586088c071e629e862119ceb00e236a79777fc
apere258 <165004346+apere258@users.noreply.github.com>
2025-06-23 10:52:43 -0500

    Feat: Created a new swift file "viewModel" to store company information and added location.

--- CIRN-2025/Manufacturing Companies/Model/Company.swift
+++ CIRN-2025/Manufacturing Companies/Model/Company.swift
@@ -6,20 +6,12 @@
 //
 
 import Foundation
-class ManufacturingViewModel {
-    //struck that creates a variable that could store multiple infromation inside
+    //struct that creates a variable that could store multiple infromation inside
     //Allowing to add multiple things in one line
     struct Company: Identifiable {
         let id = UUID()
         let name: String
         let description: String
         let email: String
+        let location: String
     }
-    var companys: [Company] = [
-        Company(name: "Company", description: "++++", email: "Contact info"),
-        Company(name: "Archer-Daniels-Midland Company", description: "++++", email: "Contact info"),
-        Company(name: "AbbVie Inc.", description: "++++", email: "Contact info"),
-        Company(name: "Mondelez International, Inc.", description: "++++", email: "Contact info"),
-        Company(name: "GE HealthCare Technologies Inc.", description: "++++", email: "Contact info")
-    ]
-}
---
+++ CIRN-2025/Manufacturing Companies/Model/ManufacturingViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  File.swift
+//  CIRN-2025
+//
+//  Created by Amado Perez - on 6/23/25.
+//
+
+import Foundation
+
+class ManufacturingViewModel {
+    var companys: [Company] = [
+        Company(name: "Kellanova", description: "Snack & cereal maker, global", email: "investor.relations@kellanova.com", location: "Chicago, IL"),
+        Company(name: "Archer-Daniels-Midland Company", description: "Agriculture & nutrition leader", email: "admgrainmarketingservices@adm.com;", location: "Decatur, IL"),
+        Company(name: "AbbVie Inc.", description: "Biopharma innovator", email: "abbviemediarelations@abbvie.com", location: "North Chicago, IL"),
+        Company(name: "Mondelez International, Inc.", description: "Global snacks company", email: "HRconnect@mdlz.com", location: "Chicago, IL (Deerfield HQ)"),
+        Company(name: "GE HealthCare Technologies Inc.", description: "Medical tech & imaging", email: "TeamGEHealthcare@gehealthcare.com", location: "Chicago IL"),
+        Company(name: "Conagra Brands, Inc.", description: "Packaged foods", email: "media@conagra.com", location: "Chicago, IL"),
+        Company(name: "Molson Coors Beverage Company", description: "Major brewer", email: "MCBCInvestorRelations@molsoncoors.com", location: "Chicago, IL"),
+        Company(name: "Motorola Solutions, Inc.", description: "Mission-critical tech", email: "partners.na@motorolasolutions.com", location: "Chicago, IL"),
+        Company(name: "R. R. Donnelley & Sons Company", description: "Printing & packaging", email: "1-800-782-4892", location: "Chicago, IL"),
+        Company(name: "Amsted Industries Incorporated", description: "Heavy-industry manufacturer", email: "312-645-1700", location: "Chicago IL"),
+        Company(name: "Marmon Holdings, Inc.", description: "Diversified manufacturing", email: "312-372-9500", location: "Chicago, IL")
+    ]
+}
+
