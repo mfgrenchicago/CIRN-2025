a792118ed637f47badf3827656fff1788374347a
Nerdy Yawdie <di_muirhead@hotmail.com>
2025-06-26 08:47:14 -0400

    feat: Add CustomWebView to be able to show websites in the app

---
+++ CIRN-2025/Shared Views/CustomWebView.swift
@@ -0,0 +1,28 @@
+//
+import WebKit
+import SwiftUI
+
+// Custom WebView for loading websites based on a url
+// TODO: Add `iOS26` new SwiftUI WebView
+struct CustomWebView: UIViewRepresentable {
+	
+	// Website url passed to load in the webview
+	let url: URL
+    
+	func makeUIView(context: Context) -> WKWebView {
+		return WKWebView()
+	}
+	
+	func updateUIView(_ webView: WKWebView, context: Context) {
+		// Create `urlRequest` using the `url`
+		let request = URLRequest(url: url)
+		
+		// Show the website in the webview
+		webView.load(request)
+	}
+}
+
+#Preview {
+	
+	CustomWebView(url: URL(string: "https://sarunw.com/posts/swiftui-webview/")!)
+}
