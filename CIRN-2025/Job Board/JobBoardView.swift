1be12e268afcdafa1fd6cf94338c73bb25df63de
lmfeurer <lmfeurer@gmail.com>
2025-07-11 16:15:53 -0500

    Updated the job board API handling logic

--- CIRN-2025/Job Board/JobBoardView.swift
+++ CIRN-2025/Job Board/JobBoardView.swift
@@ -8,28 +8,46 @@
 import SwiftUI
 struct JobBoardView: View {
 	
-	var viewModel = JobBoardViewModel()
+	@StateObject var viewModel = JobBoardViewModel()
 	
 	var body: some View {
 		NavigationStack {
-			List(viewModel.jobs) { job in
-				NavigationLink {
-					JobBoardDetailsView(job: job)
-				} label: {
-					JobBoardRowView(job: job)
-					
-				}
-			}
-			.listStyle(.plain)
-			.navigationTitle("Job Boards")
-			.toolbar {
-				ToolbarItem(placement: .navigationBarTrailing) {
-					Button("Share") {
-						// Action here
-						print("Post Job button tapped")
-					}.bold()
-				}
-			}
+            VStack {
+                if !viewModel.jobs.isEmpty {
+                    List(viewModel.jobs) { job in
+                        NavigationLink {
+                            JobBoardDetailsView(job: job)
+                        } label: {
+                            JobBoardRowView(job: job)
+                            
+                        }
+                    }
+                    .listStyle(.plain)
+                    .navigationTitle("Job Boards")
+                    .toolbar {
+                        ToolbarItem(placement: .navigationBarTrailing) {
+                            Button("Share") {
+                                // Action here
+                                print("Post Job button tapped")
+                            }.bold()
+                        }
+                    }
+                } else {
+                    ContentUnavailableView(LocalizedStringKey("No jobs"), systemImage: "magnifyingglass", description: Text("There are no jobs available at this time"))
+                }
+                    
+            }
+            .onAppear {
+                viewModel.fetchJobs()
+            }
+            .overlay {
+                if viewModel.isLoading == true {
+                    ProgressView()
+                }
+            }
+            .alert(isPresented: $viewModel.showErrorAlert) {
+                Alert(title: Text("Check back later"))
+            }
 		}
 	}
 }
